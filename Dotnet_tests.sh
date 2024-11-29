7b3b891ef7cdeb6e5b5bee6b26de9fdb79bf9d23
Swagger-IC <100158344+Swagger-IC@users.noreply.github.com>
2024-11-29 12:32:13 +0100

    Update Dotnet_tests.sh

--- Dotnet_tests.sh
+++ Dotnet_tests.sh
@@ -108,12 +108,12 @@ GIT_COMMIT_HASH=$(git rev-parse --short HEAD)
 docker build -t dotnet:$GIT_COMMIT_HASH tempdir
 
 # Stop and remove any running container with the same name
-if docker ps -a --filter "name=dotnetapp" --format '{{.Names}}' | grep -q dotnetapp; then
-  docker rm -f dotnetapp
+if docker ps -a --filter "name=dotnettests" --format '{{.Names}}' | grep -q dotnettests; then
+  docker rm -f dotnettests
 fi
 
 # Run the new container
-docker run -t -d -p 6000:5000 --name dotnetapp dotnet:$GIT_COMMIT_HASH
+docker run -t -d -p 6000:5000 --name dotnettests dotnettests:$GIT_COMMIT_HASH
 
 # List the running Docker containers
-docker ps -a | grep dotnetapp
+docker ps -a | grep dotnettests
