301ef3db780a0dc32e94605dc7ac316d91b6fa18
Xander Dheedene <xander.dheedene@student.hogent.be>
2024-11-29 12:19:05 +0100

    Update Dotnet_build_swarm.sh

--- Dotnet_build_swarm.sh
+++ Dotnet_build_swarm.sh
@@ -41,6 +41,8 @@ done
 # Copy the solution file to tempdir
 cp Rise.sln tempdir
 
+sed -i 's|^\s*"SqlServer": *".*"|    "SqlServer": "Server=192.168.56.11,1433;Database=Hogent.RiseDb;User Id=sa;Password=Password1234!;Encrypt=Optional;TrustServerCertificate=true"|' tempdir/Rise.Server/appsettings.json
+
 # Create the Dockerfile
 cat > tempdir/Dockerfile << _EOF_
 # Use aspnet for .NET runtime
