4c95d5fe7243a60497f5211242f69fffc853b8a0
Xander Dheedene <xander.dheedene@student.hogent.be>
2024-12-03 13:49:32 +0100

    Update Save_container_and_scp.sh

--- Save_container_and_scp.sh
+++ Save_container_and_scp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 GIT_COMMIT_HASH=$(git rev-parse --short HEAD)
 remote_server="192.168.56.13"
-SSH_connection="root@$remote_server"
+SSH_connection="vagrant@$remote_server"
 
 # save the container to a tarball
 docker save dotnet:$GIT_COMMIT_HASH > dotnet.tar
 
 #Transfer the tarball to the remote server
-scp dotnet.tar $SSH_connection:/root/
+scp -i /var/jenkins_home/.ssh/id_rsa dotnet.tar $SSH_connection:/home/vagrant/
 
 # Remove the tarball
 rm dotnet.tar
