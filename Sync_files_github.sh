8877c8392034741d611ab68dee7942bad6af23b1
Swagger-IC <100158344+Swagger-IC@users.noreply.github.com>
2024-12-03 16:50:26 +0100

    Update Sync_files_github.sh

--- Sync_files_github.sh
+++ Sync_files_github.sh
@@ -18,3 +18,41 @@ rsync -av --delete \
   --exclude '.git/' \
   --exclude '*.sh' \
   ./ tempdir/
+
+  #!/bin/bash
+set -euo pipefail
+
+# Clean up unused Docker resources
+docker system prune -f
+
+# Create the temporary directory if it doesn't exist
+if [ ! -d tempdir ]; then
+  mkdir -p tempdir
+fi
+
+# Sync files, excluding unnecessary ones
+rsync -av --delete \
+  --exclude 'bin/' \
+  --exclude 'obj/' \
+  --exclude 'README.md' \
+  --exclude '.gitignore' \
+  --exclude '.git/' \
+  --exclude '*.sh' \
+  --exclude 'old_scripts/'
+  --exclude 'Jenkinsfile' \
+  ./ tempdir/
+
+# Build the Docker image, from the dockerfile in the tempdir
+GIT_COMMIT_HASH=$(git rev-parse --short HEAD)
+docker build -t dotnet:$GIT_COMMIT_HASH tempdir
+
+# Stop and remove any running container with the same name
+if docker ps -a --filter "name=dotnetapp" --format '{{.Names}}' | grep -q dotnetapp; then
+  docker rm -f dotnetapp
+fi
+
+# Run the new container
+docker run -t -d -p 6000:5000 --name dotnetapp dotnet:$GIT_COMMIT_HASH
+
+# List the running Docker containers
+docker ps -a | grep dotnetapp
