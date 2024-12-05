166351e3c09cb056fcc7ff9ee294a9eb6bca766c
Swagger-IC <100158344+Swagger-IC@users.noreply.github.com>
2024-12-05 17:11:32 +0100

    Update Unpack_and_swarm.sh

--- Unpack_and_swarm.sh
+++ Unpack_and_swarm.sh
@@ -16,7 +16,7 @@ if ! docker info | grep -q 'Swarm: active'; then
 fi
 
 #unpack the tarball
-docker load < dotnet_$GIT_COMMIT_HASH.tar
+docker load < /home/dotnet_$GIT_COMMIT_HASH.tar
 
 # Deploy or update the service
 if docker service ls --format '{{.Name}}' | grep -q "^$SERVICE_NAME\$"; then
