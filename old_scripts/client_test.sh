dfba996a1754aa8ba06b8462b70b84c093ce6b33
Swagger-IC <100158344+Swagger-IC@users.noreply.github.com>
2024-12-06 12:21:12 +0100

    Update client_test.sh

--- old_scripts/client_test.sh
+++ old_scripts/client_test.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 GIT_COMMIT_HASH=$(git rev-parse --short HEAD)
 
+# Build the Docker image with the latest commit hash
+echo "Building Docker image..."
+docker build -t dotnet:$GIT_COMMIT_HASH .
+
 # Run Client Tests
 echo "Running Client Tests"
-docker run -t -v $PWD:/app -w /app/Rise.Client.Tests dotnet:$GIT_COMMIT_HASH dotnet test --no-build --logger:trx
+docker run --rm -v $PWD:/app -w /app/Rise.Client.Tests dotnet:$GIT_COMMIT_HASH dotnet test --no-build --logger:trx
